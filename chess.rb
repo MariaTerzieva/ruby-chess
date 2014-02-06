2984601e24250de19085d8c1519950cf40891269
Maria Terzieva <mimi_t92@abv.bg>
2014-02-06 17:06:20 +0200

    Modify Knight class

--- chess.rb
+++ chess.rb
@@ -129,10 +129,10 @@ class Knight < Piece
     horizontal = from.delta_x(to) == 2 and from.delta_y(to) == 1
     vertical = from.delta_x(to) == 1 and from.delta_y(to) == 2
     return false unless vertical or horizontal
-    @board.king_remains_safe_after_move?(from, to)
+    @board.king_remains_safe_after_move? from, to
   end
 
-  def empty_or_opponent_on(position)
+  def empty_or_opponent_on?(position)
     @board.empty?(position) or @board.color_of_piece_on(position) != color
   end
 
@@ -141,12 +141,11 @@ class Knight < Piece
                  [from.x + 2, from.y - 1], [from.x + 1, from.y - 2],
                  [from.x - 1, from.y + 2], [from.x - 2, from.y + 1],
                  [from.x - 1, from.y - 2], [from.x - 2, from.y - 1]]
-    positions.each do |position|
-      next unless position.all? { |coordinate| coordinate.between?(0, 7) }
+    positions.any? do |position|
       position = Square.new(*position)
-      return true if empty_or_opponent_on(position) and valid_move?(from, position)
+      next unless position.inside_the_board?
+      empty_or_opponent_on?(position) and valid_move?(from, position)
     end
-    false
   end
 end
 
