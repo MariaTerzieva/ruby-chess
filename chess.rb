2519decff14b08d4dcb6888530a2c28f8e144eae
Maria Terzieva <mimi_t92@abv.bg>
2014-02-06 19:17:52 +0200

    Modify Rook class

--- chess.rb
+++ chess.rb
@@ -325,13 +325,13 @@ class Rook < Piece
     dx = to.x <=> from.x
     dy = to.y <=> from.y
     steps = [from.delta_x(to), from.delta_y(to)].max
-    return false if obstructions?(dx, dy, steps, from)
-    @board.king_remains_safe_after_move?(from, to)
+    return false if obstructions? dx, dy, steps, from
+    @board.king_remains_safe_after_move? from, to
   end
 
   def any_moves?(from)
     in_directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-    super(from, in_directions)
+    super from, in_directions
   end
 
   def move(from, to)
